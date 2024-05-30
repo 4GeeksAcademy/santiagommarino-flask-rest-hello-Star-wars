d9e08b360e55714a34ab06eaf0a75cb829f82b34
santiagommarino <153780787+santiagommarino@users.noreply.github.com>
2024-05-30 21:01:25 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -7,8 +7,11 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
 
-    def __repr__(self):
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
+
+    def to_dict(self):
         return '<User %r>' % self.username
 
     def serialize(self):
@@ -17,3 +20,21 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    climate = db.Column(db.String(100))
+    terrain = db.Column(db.String(100))
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    birth_year = db.Column(db.String(100))
+    gender = db.Column(db.String(100))
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
